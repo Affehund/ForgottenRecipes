52a99820b946964df31ff829e58041bd3a803436
Affehund <affehund123@web.de>
2021-09-08 22:43:36 +0200

    removed light recipe as the light block is not usable in survival mode

--- src/main/java/com/affehund/forgottenrecipes/ForgottenRecipes.java
+++ src/main/java/com/affehund/forgottenrecipes/ForgottenRecipes.java
@@ -1,35 +1,11 @@
 package com.affehund.forgottenrecipes;
 
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
-@Mod(ForgottenRecipes.MOD_ID)
+@Mod("forgottenrecipes")
 public class ForgottenRecipes
 {
-    private static final Logger LOGGER = LogManager.getLogger();
-    public static final String MOD_ID = "forgottenrecipes";
-    final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-
     public ForgottenRecipes() {
-        // ITEMS.register(modEventBus);
-        // BLOCKS.register(modEventBus);
-
-        MinecraftForge.EVENT_BUS.register(this);
     }
-
-    /*
-    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS,
-            "minecraft");
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS,
-            "minecraft");
-
-    public static final RegistryObject<Block> LIGHT_BLOCK = BLOCKS.register("light", () -> new LightBlock(BlockBehaviour.Properties.of(Material.AIR).strength(0F, 3600000.8F).noOcclusion().lightLevel(LightBlock.LIGHT_EMISSION)));
-
-    public static final RegistryObject<Item> LIGHT = ITEMS.register("light", () -> new BlockItem(LIGHT_BLOCK.get(), new Item.Properties().tab(CreativeModeTab.TAB_DECORATIONS).rarity(Rarity.UNCOMMON)));
-     */
 }
 
